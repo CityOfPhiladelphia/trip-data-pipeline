bb3656843b1cca5e8379a0b31ac9ae6ed30a5713
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2016-08-24 17:38:15 -0400

    Add an upload script that adds the data to a database table

--- README.md
+++ README.md
@@ -1,29 +1,34 @@
 Taxi Trip Data
 ==============
 
+Scripts to clean up
+
 ## Installation
 
 pip install https://github.com/CityOfPhiladelphia/taxi-trip-data-pipeline.git#egg=taxi-trip-data-pipeline
 
 
+### Development
+
+pip install -e .
+
+
 ## Usage
 
 ```bash
-python taxitrips.py --help
+taxitrips.py --help
 # Usage: taxitrips.py [OPTIONS]
 #
 # Options:
 #   -v, --verifone PATH  Verifone data files
 #   -c, --cmt PATH       CMT data files
 #   --help               Show this message and exit.
-```
 
-**Example:**
+# EXAMPLES:
 
-```bash
 # Save result to a file:
-taxitrips.py -v "testdata/verifone*" -c "testdata/cmt*" > output/merged.csv
+taxitrips.py transform -v "testdata/verifone*" -c "testdata/cmt*" > testdata/merged.csv
 
 # Send result to standard output:
-taxitrips.py -v "testdata/verifone*" -c "testdata/cmt*" 2> /dev/null
+taxitrips.py transform -v "testdata/verifone*" -c "testdata/cmt*" 2> /dev/null
 ```
--- taxitrips.py
+++ taxitrips.py
@@ -32,10 +32,7 @@ def transform(verifone_filenames, cmt_filenames):
        Verifone
     3. Remove Shift # column.
     4. Remove Device Type column.
-    5. Format Columns R, S, T, U, V, and W to be 4 decimal points and currency.
-
-       TODO: ^^^^ CHECK ON THAT ONE; DOESN'T SOUND RIGHT. WHY 4 DECIMALS? ^^^^
-
+    5. Format Columns R, S, T, U, V, and W to be 2 decimal points and currency.
     6. Anonymize the 'Medallion #' fields or remove that column entirely and
        instead create a column with a unique ID to represent each driver,
        based on the 'Chauffeur #' field in the CMT and Verifone files.
@@ -49,6 +46,7 @@ def transform(verifone_filenames, cmt_filenames):
 
     t = petl.cat(t_ver, t_cmt)\
         .cutout('Shift #')\
+        .cutout('Device Type')\
         .convert('Fare', asmoney)\
         .convert('Tax', asmoney)\
         .convert('Tips', asmoney)\
@@ -59,10 +57,66 @@ def transform(verifone_filenames, cmt_filenames):
     return t
 
 
+def upload(table_filename, username, password, db_conn_string, t_func=lambda t: t):
+    """
+    Load a merged taxi trips table from a CSV file into the database.
+    """
+    import cx_Oracle
+    conn = cx_Oracle.connect(username, password, db_conn_string)
+    cursor = conn.cursor()
+
+    t = petl.fromcsv(table_filename)\
+        .setheader(['Trip_No', 'Operator_Name', 'Medallion', 'Chauffeur_No', 'Meter_On_Datetime', 'Meter_Off_Datetime', 'Trip_Length', 'Pickup_Latitude', 'Pickup_Longitude', 'Pickup_Location', 'Dropoff_Latitude', 'Dropoff_Longitude', 'Dropoff_Location', 'Fare', 'Tax', 'Tips', 'Tolls', 'Surcharge', 'Trip_Total', 'Payment_Type', 'Street_or_Dispatch', 'Data_Source'])
+    todb_upsert(t_func(t), cursor)
+    conn.commit()
+    conn.close()
+
+    return t
+
+
+def grouper(n, iterable, fillvalue=None):
+    "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
+    from itertools import zip_longest
+    args = [iter(iterable)] * n
+    return zip_longest(fillvalue=fillvalue, *args)
+
+
+def todb_upsert(table, cursor):
+    # Create a list of the column names
+    columns = table.fieldnames()
+
+    # Build the clauses for the SQL statement
+    select_clause = 'SELECT {} FROM DUAL'.format(
+        ', '.join(':{0} AS {0}'.format(c) for c in columns))
+    update_clause = 'UPDATE SET {}'.format(
+        ', '.join('orig.{0} = new.{0}'.format(c) for c in columns if c != 'Trip_No'))
+    insert_clause = 'INSERT ({}) VALUES ({})'.format(
+        ', '.join('orig.{}'.format(c) for c in columns),
+        ', '.join('new.{}'.format(c) for c in columns))
+
+    sql = '''
+    MERGE INTO taxi_trips orig
+        USING ({}) new
+        ON (orig.Trip_No = new.Trip_No)
+        WHEN MATCHED THEN {}
+        WHEN NOT MATCHED THEN {}
+    '''.format(select_clause, update_clause, insert_clause)
+
+    for row_group in grouper(100, table.values(columns)):
+        row_group = filter(None, row_group)
+        cursor.executemany(sql, list(row_group))
+
+
+
 # ============================================================
 
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+
+@cli.command(name='transform')
 @click.option('--verifone', '-v', type=click.Path(), multiple=True, help='Verifone data files')
 @click.option('--cmt', '-c', type=click.Path(), multiple=True, help='CMT data files')
 def transform_cmd(verifone, cmt):
@@ -71,5 +125,14 @@ def transform_cmd(verifone, cmt):
         .tocsv()
 
 
+@cli.command(name='upload')
+@click.option('--username', '-u', help='The database username')
+@click.option('--password', '-p', help='The database user password')
+@click.option('--dsn', '-d', help='The database DSN connection script')
+@click.argument('csv_filename')
+def upload_cmd(csv_filename, username, password, dsn):
+    upload(csv_filename, username, password, dsn, t_func=lambda t: t.progress())
+
+
 if __name__ == '__main__':
-    transform_cmd()
+    cli()
